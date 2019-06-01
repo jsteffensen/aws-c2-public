9b9b7f9a0d9ef064209e30b3c5c784c86d3192c5
jsteffensen <jsteffensen@gmail.com>
2019-06-01 12:50:38 +0200

    add monitoring setup

--- monitoring.sh
+++ monitoring.sh
@@ -1,24 +1,28 @@
 # Install Docker
 sudo yum install -y docker
 sudo systemctl start docker.service
-
-# add user to docker group - log and back in for user to pick up these privileges
 sudo usermod -a -G docker ec2-user
-
-# install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Test Zoonavigator
+docker-compose -f zoonavigator.yml up -d
 # Setup Docker Compose as systemD
 sudo wget -P /etc/systemd/system/ https://raw.githubusercontent.com/jsteffensen/aws-c2-public/master/docker-compose%40.service
-
 # Install Zoonavigator as SystemD
 sudo mkdir -p /etc/docker/compose/zoonavigator/
-sudo wget -P /etc/docker/compose/zoonavigator/ -O docker-compose.yml https://raw.githubusercontent.com/jsteffensen/aws-c2-public/master/zoonavigator.yml
+sudo nano /etc/docker/compose/zoonavigator/docker-compose.yml
+sudo systemctl enable docker-compose@zoonavigator
+sudo systemctl start docker-compose@zoonavigator
 
+# Test Kafka Manager
+docker-compose -f kafka-manager.yml up -d
 # Install Kafka Manager as SystemD
 sudo mkdir -p /etc/docker/compose/kafka-manager/
-sudo wget -P /etc/docker/compose/kafka-manager/ -O docker-compose.yml https://raw.githubusercontent.com/jsteffensen/aws-c2-public/master/kafka-manager.yml
+sudo wget -P /etc/docker/compose/kafka-manager/ https://raw.githubusercontent.com/jsteffensen/aws-c2-public/master/kafka-manager.yml
+sudo mv /etc/docker/compose/kafka-manager/kafka-manager.yml /etc/docker/compose/kafka-manager/docker-compose.yml
+sudo systemctl enable docker-compose@kafka-manager # automatically start at boot
+sudo systemctl start docker-compose@kafka-manager   
 
 # Install Kafka-Monitor
 sudo yum install -y git
@@ -26,14 +30,13 @@ git clone https://github.com/linkedin/kafka-monitor.git
 cd kafka-monitor
 sudo yum install -y java-1.8.0-openjdk-devel
 ./gradlew jar
-./bin/kafka-monitor-start.sh config/kafka-monitor.properties
-
+# to test run:
+# ./bin/kafka-monitor-start.sh config/kafka-monitor.properties
 # Setup as SystemD component
 sudo wget -P /etc/systemd/system/ https://raw.githubusercontent.com/jsteffensen/aws-c2-public/master/kafka-monitor.service
 
-# Automatically start at boot
-#sudo systemctl enable docker-compose@kafka-manager 
+#must be run from kafka-monitor directory https://github.com/linkedin/kafka-monitor/issues/116#issuecomment-407606327
+# ./bin/kafka-monitor-start.sh /config/kafka-monitor.properties
 
-# Start services
-sudo systemctl start docker-compose@zoonavigator
-sudo systemctl start docker-compose@kafka-manager   
+# Run as service:
+# sudo systemctl start kafka-monitor
